eb6a2bbd993d575056ef443e70187bfa5a61c154
Dhineshkumarraveendiran <46440489+Dhineshkumarraveendiran@users.noreply.github.com>
2019-01-30 15:41:44 +0530

    Create 21.py

---
+++ 21.py
@@ -0,0 +1,11 @@
+#q
+def sumofAP1(n2,a2,d2):
+    su1=0
+    i=0
+    while i<n2:
+        su1+=a2
+        a2+=d2
+        i+=1
+    return su1    
+n2,a2,d2=map(int,input().split())
+print(sumofAP1(n2,a2,d2))
