5c94f520dbb0196d4da9bd61daf54d5d454f005b
Dhineshkumarraveendiran <46440489+Dhineshkumarraveendiran@users.noreply.github.com>
2019-01-31 16:01:37 +0530

    Create player11!!.py

---
+++ player11!!.py
@@ -0,0 +1,16 @@
+#q
+n=input()
+if n=='Monday':
+  print("no")
+elif n=='Tuesday':
+  print("no")
+elif n=='Wednesday':
+  print("no")
+elif n=='Thursday':
+  print("no")
+elif n=='Friday':
+  print("no")
+elif n=='Saturday':
+  print("yes")
+else:
+  print("yes")
